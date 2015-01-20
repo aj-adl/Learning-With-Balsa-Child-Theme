74cad3a1ee5ebe0b42002ede94c669606600ff34
aj-adl <alexjamesbishop@gmail.com>
2015-01-20 16:48:18 +1030

    Register CPT + Remove customizer css

--- functions.php
+++ functions.php
@@ -1,8 +1,64 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css/main.css', array( 'parent-style' ) );
 }
+
+// Remove customizer CSS because it's awful and hard to overide. 
+
+add_action(wp_head, remove_make_custom_css, 5);
+
+function remove_make_custom_css() {
+		remove_action('wp_head', 'ttfmake_display_customizations', 11 );
+	}
+
+// Register Custom Post Type
+function create_project_post_type() {
+
+	$labels = array(
+		'name'                => 'projects',
+		'singular_name'       => 'Project',
+		'menu_name'           => 'Projects',
+		'parent_item_colon'   => 'Parent Item:',
+		'all_items'           => 'All Items',
+		'view_item'           => 'View Item',
+		'add_new_item'        => 'Add New Project',
+		'add_new'             => 'Add New',
+		'edit_item'           => 'Edit Item',
+		'update_item'         => 'Update Item',
+		'search_items'        => 'Search Projects',
+		'not_found'           => 'Not found',
+		'not_found_in_trash'  => 'Not found in Trash',
+	);
+	$args = array(
+		'label'               => 'Project',
+		'description'         => 'A Learning With Balsa Project',
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields', 'author'),
+		'taxonomies'          => array( 'category', 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'project', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'create_project_post_type', 0 );
+
+add_post_type_support( 'project', 'make-builder');
+
 ?>
